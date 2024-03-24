12e0190a4beb77d5ed28c86ae35dc359129a809e
Sawna <smakhnovskiy19@gmail.com>
2024-03-24 22:16:08 +0100

    Added registration and login form.

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {LoginComponent} from "../login-form/login.component";
+import {AxiosService} from "../services/axios.service";
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [
+    LoginComponent
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+  constructor(private axiosService: AxiosService) {}
+  onLogin(input: any){
+    this.axiosService.request(
+      "POST",
+      "/api/users/login",
+      {email: input.email,
+      password: input.password}
+    ).then(response => {
+      this.axiosService.setAuthToken(response.data.token);
+    })
+  }
+}
