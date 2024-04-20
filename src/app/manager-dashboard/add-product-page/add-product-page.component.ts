defb8fd92b46d54d40212800f35b8c448db49a91
good <good@quadrk.space>
2024-04-20 23:33:37 +0200

    Added components for future work

--- src/app/manager-dashboard/add-product-page/add-product-page.component.ts
+++ src/app/manager-dashboard/add-product-page/add-product-page.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
-import {ToastService} from "angular-toastify";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-add-product-page',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatButton,
+    MatProgressSpinner
   ],
   templateUrl: './add-product-page.component.html',
   styleUrl: './add-product-page.component.css'
@@ -23,4 +31,5 @@ export class AddProductPageComponent {
     price: ['', Validators.required]
   });
 
+  protected readonly name = name;
 }
