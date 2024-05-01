413fcdb0ab6c04c31a88f3146050d07a3539258f
quadrk <quadrk@pm.me>
2024-05-01 19:25:10 +0200

    Added footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {RouterModule, RouterOutlet} from '@angular/router';
 import {AngularToastifyModule} from "angular-toastify";
 import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
 import {HeaderComponent} from "./header/header.component";
+import { FooterComponent } from './footer/footer.component';
 import {FormsModule} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
 
@@ -10,7 +11,7 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, FormsModule ,AngularToastifyModule, MatButtonToggleGroup, MatButtonToggle, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, FormsModule ,AngularToastifyModule, MatButtonToggleGroup, MatButtonToggle, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
