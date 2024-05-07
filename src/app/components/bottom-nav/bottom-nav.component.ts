901bedccec7993a9d81a9a65fcffd4ac2c6868e4
quadrk <quadrk@pm.me>
2024-05-08 01:02:54 +0200

    Add bottom navigation component for mobile devices

---
+++ src/app/components/bottom-nav/bottom-nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, NgZone, OnDestroy } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-bottom-nav',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './bottom-nav.component.html',
+  styleUrl: './bottom-nav.component.css'
+})
+export class BottomNavComponent {
+  // v1.0.0 icons
+  cartIconPath = 'assets/icons/cart-icon.svg';
+  customerIconPath = 'assets/icons/customer-icon.svg';
+  loginIconPath = 'assets/icons/login-icon.svg';
+
+  isLoggedIn = false;
+  private authSubscription: Subscription;
+
+  constructor(private authService: AuthService, private zone: NgZone) {
+    this.authSubscription = this.authService.isLoggedIn.subscribe(status => {
+      this.zone.run(() => { // Используйте NgZone для обновления UI
+        this.isLoggedIn = status;
+        console.log("Login status changed:", status);
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+}
