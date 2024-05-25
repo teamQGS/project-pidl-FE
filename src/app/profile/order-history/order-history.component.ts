a49a6718c6e6a9482b398b5b357b20c19933beb5
good <good@quadrk.space>
2024-05-25 17:03:04 +0200

    Orders history for users done

--- src/app/profile/order-history/order-history.component.ts
+++ src/app/profile/order-history/order-history.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {ProductsDTO} from "../../model/products";
+import {MatButton} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {OrderDTO} from "../../model/orders";
+import {FormBuilder} from "@angular/forms";
+import {OrderService} from "../../services/orders/order.service";
 
 @Component({
   selector: 'app-order-history',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    RouterLink,
+    MatButton,
+    MatFormField,
+    MatLabel,
+    MatOption,
+    MatSelect
+  ],
   templateUrl: './order-history.component.html',
   styleUrl: './order-history.component.css'
 })
-export class OrderHistoryComponent {
+export class OrderHistoryComponent implements OnInit {
+  orders: OrderDTO[] = [];
+  products: ProductsDTO[] = [];
+  selectedStatus: never[] = [];
+  username = window.localStorage.getItem('username');
 
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    this.orderService.getCustomersOrders().then((orders: OrderDTO[]) => {
+      this.orders = orders;
+      // @ts-ignore
+      this.selectedStatus = this.orders.map(order => order.status);
+    }).catch((error: any) => {
+      console.error('Error while fetching products:', error);
+    });
+  }
+
+  formatDate(dateString: Date): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-GB'); // 'en-GB' форматирует дату как dd/mm/yyyy
+  }
 }
--- src/app/services/orders/order.service.ts
+++ src/app/services/orders/order.service.ts
@@ -10,7 +10,7 @@ import {ProductsDTO} from "../../model/products";
   providedIn: 'root'
 })
 export class OrderService {
-
+  username = window.localStorage.getItem('username');
   constructor(private axiosService: AxiosService, private snackBar: MatSnackBar) { }
 
   async getAllOrders(): Promise<any[]> {
@@ -18,6 +18,21 @@ export class OrderService {
       const response = await this.axiosService.request(
         'GET',
         'http://localhost:8080/api/manager/orders',
+        {}
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error while fetching orders:', error);
+      return [];
+    }
+  }
+
+  async getCustomersOrders(): Promise<any[]> {
+    try {
+      const response = await this.axiosService.request(
+        'GET',
+        `http://localhost:8080/api/orders/findAll/${(this.username)}`,
         {}
       );
       console.log(response.data);
