01e28368785b1ebe10a1a46e18c2e2c2d43e93ad
Artur Iunusov <artur.iunusov@student.umb.sk>
2024-05-26 19:12:39 +0200

    added feedback logic

--- src/app/header/contact-us/contact-us.component.ts
+++ src/app/header/contact-us/contact-us.component.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
+import { AxiosService } from '../../services/axios/axios.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import {MatButton} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-contact-us',
-  standalone: true,
-  imports: [],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatOption,
+    MatSelect,
+    ReactiveFormsModule
+  ],
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  constructor(
+    private axiosService: AxiosService,
+    private snackBar: MatSnackBar,
+    private formBuilder: FormBuilder
+  ) {}
 
+  contactUsForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    subject: [ '', Validators.required],
+    feedbackContent: [ '', Validators.required]
+  });
+
+  onSubmit() {
+    const formattedDate = this.formatDate(new Date());
+    this.axiosService.request(
+        "POST",
+        "/api/feedback/add",
+        {
+          username: this.contactUsForm.value.username,
+          email: this.contactUsForm.value.email,
+          subject: this.contactUsForm.value.subject,
+          feedbackContent: this.contactUsForm.value.feedbackContent,
+          date: formattedDate
+        }
+      ).then(response => {
+        this.snackBar.open("Message sent successfully", '', {
+          duration: 3000
+        });
+      }).catch(error => {
+        console.log('error', error);
+        this.snackBar.open("Error occurred while sending message", '', {
+          duration: 3000
+        });
+      });
+    }
+  private formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year}`;
+  }
 }
---
+++ src/app/services/feedback/feedback-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {ProductsDTO} from "../../model/products";
+import {AxiosService} from "../axios/axios.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedbackServiceService {
+
+  constructor(private axiosService: AxiosService) { }
+
+  async getAll(): Promise<ProductsDTO[]> {
+    try {
+      const response = await this.axiosService.request(
+        'GET',
+        '/api/products',
+        {}
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error while fetching products:', error);
+      return [];
+    }
+  }
+}
