eb156c07542a20ab84a7347164b481075ccdfc01
quadrk <quadrk@pm.me>
2024-05-27 00:35:07 +0200

    Improvements 0.2.9: Fixes & Styles

--- src/app/profile/update-user-form/update-user-form.component.ts
+++ src/app/profile/update-user-form/update-user-form.component.ts
@@ -1,15 +1,15 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {SettingsComponent} from "../settings/settings.component";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
-import {AxiosService} from "../../services/axios/axios.service";
-import {Router} from "@angular/router";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { SettingsComponent } from "../settings/settings.component";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AxiosService } from "../../services/axios/axios.service";
+import { Router } from "@angular/router";
 import { RouterLink } from '@angular/router';
-import {MatInput} from "@angular/material/input";
-import {MatButton} from "@angular/material/button";
-import {MatTooltip} from "@angular/material/tooltip";
-import {MatIcon} from "@angular/material/icon";
-import {NgIf} from "@angular/common";
+import { MatInput } from "@angular/material/input";
+import { MatButton } from "@angular/material/button";
+import { MatTooltip } from "@angular/material/tooltip";
+import { MatIcon } from "@angular/material/icon";
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-update-user-form',
@@ -28,7 +28,9 @@ import {NgIf} from "@angular/common";
   templateUrl: './update-user-form.component.html',
   styleUrl: './update-user-form.component.css'
 })
-export class UpdateUserFormComponent implements OnInit{
+export class UpdateUserFormComponent implements OnInit {
+  isEditEnabled: boolean = false;
+
   constructor(private fb: FormBuilder, private axiosService: AxiosService,
               private router: Router) { }
 
@@ -38,7 +40,7 @@ export class UpdateUserFormComponent implements OnInit{
     this.axiosService.request(
       "GET",
       `/api/users/find/${this.username}`,
-    {}
+      {}
     ).then(response => {
       const userData = response.data;
       this.updateForm.patchValue({
@@ -54,17 +56,22 @@ export class UpdateUserFormComponent implements OnInit{
   }
 
   updateForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    email: [''],
-    address: [''],
-    phoneNumber: ['']
+    firstName: [{ value: '', disabled: true }],
+    lastName: [{ value: '', disabled: true }],
+    email: [{ value: '', disabled: true }],
+    address: [{ value: '', disabled: true }],
+    phoneNumber: [{ value: '', disabled: true }]
   });
+
+  enableEdit(): void {
+    this.isEditEnabled = true;
+    this.updateForm.enable();
+  }
 
   updateUser(): void {
     this.axiosService.request(
       'PUT',
-      `/api/users/update/${(this.username)}`,
+      `/api/users/update/${this.username}`,
       {
         firstName: this.updateForm.value.firstName,
         lastName: this.updateForm.value.lastName,
@@ -76,7 +83,6 @@ export class UpdateUserFormComponent implements OnInit{
       this.router.navigate(['/profile']);
     }).catch(error => {
       console.log('Error during update:', error);
-    })
+    });
   }
-
 }
