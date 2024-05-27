477acc84fde136d607a5beccec7944da92af784e
good <good@quadrk.space>
2024-05-27 08:37:54 +0200

    fix

--- src/app/header/contact-us/contact-us.component.ts
+++ src/app/header/contact-us/contact-us.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { AxiosService } from '../../services/axios/axios.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
 import {MatButton} from "@angular/material/button";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-contact-us',
